aebeb6c9e123fe00bf7163203f042a512350459f
zengdefeng <zengdefeng@zengdefeng.com>
2017-11-17 17:19:13 +0800

    添加物品管理

--- App/Ttfadmin/Controller/CurrencyController.class.php
+++ App/Ttfadmin/Controller/CurrencyController.class.php
@@ -130,134 +130,243 @@ class CurrencyController extends CommonController {
 		echo $str;					
 	}
 	
-	/******************/
+	/********************************************************************************/
+	//物品
+	public function goods(){		
+		
+        $goods_name = isset($_GET['goods_name'])?trim($_GET['goods_name']):'';
+        $start_time = isset($_GET['start_time'])?trim($_GET['start_time']):'';
+        $end_time = isset($_GET['end_time'])?trim($_GET['end_time']):'';
+        $search_id = isset($_GET['search_id'])?intval($_GET['search_id']):0;
+        $search_id_2 = isset($_GET['search_id_2'])?intval($_GET['search_id_2']):0;
+        $search_id_3 = isset($_GET['search_id_3'])?intval($_GET['search_id_3']):0;
+		
+		$where = '1=1';
+		if($goods_name) $where .= " and ".C('DB_PREFIX')."goods.goods_name like '%".$goods_name."%'";		
+		if($start_time) $where .= " and ".C('DB_PREFIX')."goods.goods_time >= '".$start_time."'";		
+		if($end_time) $where .= " and ".C('DB_PREFIX')."goods.goods_time < '".$end_time."'";		
+		if($search_id_3 > 0){
+			$where .= " and ".C('DB_PREFIX')."goods.goods_type = ".$search_id_3;
+		}else{
+			if($search_id_2 > 0){
+				$ids_2 = trim(D('type')->get_type_ids(D('type')->type_list($search_id_2)),',');
+				if($ids_2) $where .= " and ".C('DB_PREFIX')."goods.goods_type in (".$ids_2.")";
+		
+			}else{
+				if($search_id > 0){
+					$ids_ = trim(D('type')->get_type_ids(D('type')->type_list($search_id)),',');
+					if($ids_) $where .= " and ".C('DB_PREFIX')."goods.goods_type in (".$ids_.")";
+				}
+			}
+		}
 
-
-    //管理员列表
-    public function admin_list(){
+        $search_state['goods_name'] = $goods_name;
+        $search_state['start_time'] = $start_time;		
+        $search_state['end_time'] = $end_time;		
+        $search_state['search_id'] = $search_id;		
+        $search_state['search_id_2'] = $search_id_2;		
+        $search_state['search_id_3'] = $search_id_3;		
+		
+		$s_types['one'] = D('type')->type_list();
+		if($search_id > 0) $s_types['two'] = D('type')->type_list($search_id);
+		if($search_id_2 > 0) $s_types['three'] = D('type')->type_list($search_id_2);
+        $this->assign('s_types',$s_types);		
+		
         $page = isset($_GET['p'])?$_GET['p']:1;
-        $limit = D('admin')->get_limit($page);
-        $page_info = D('admin')->get_admin_page($page);
-        $admin_list = D('admin')->get_admin_list($limit);
-
-        $sex_state = array(
-            '0' => '未知',
-            '1' => '男',
-            '2' => '女'
-        );
-
-        $roles_arr = M('admin_role')->field('role_id,role_name')->select();
-        $roles = array();
-        $roles[0] = '超级管理员';
-        if($roles_arr){
-            foreach($roles_arr as $val){
-                $roles[$val['role_id']] = $val['role_name'];
-            }
-        }
-
-        $this->assign('roles',$roles);
-        $this->assign('sex_state',$sex_state);
-        $this->assign('admin',$admin_list);
+		$search_state['page'] = $page;
+        $limit = D('goods')->get_limit($page);
+        $page_info = D('goods')->goods_page($search_state,$where);
+        $goods_list = D('goods')->goods_list($limit,$where);
+		
+        $this->assign('search_state',$search_state);
+        $this->assign('goods_list',$goods_list);
         $this->assign('page',$page_info);
-        $this->display();
-    }
-
-
-    //添加管理员
-    public function admin_add(){
+        $this->display();	
+	}
+	
+	public function goods_add(){
         if($_POST){
-            $data['admin_account'] = trim($_POST['admin_account']);
-            $data['admin_pass'] = hex2bin(md5(trim($_POST['admin_pass'])));
-            $data['admin_role_id'] = $_POST['admin_role_id'];
-            $data['admin_register_time'] = date('Y-m-d H:i:s');
-
-            if(D('admin')->add_admin($data)){
-                $this->success('添加成功!',U('admin/admin_list'));
+			$data['goods_name'] = isset($_POST['goods_name'])?trim($_POST['goods_name']):'';
+			$goods_type = isset($_POST['goods_type'])?intval($_POST['goods_type']):0;
+			$goods_type_2 = isset($_POST['goods_type_2'])?intval($_POST['goods_type_2']):0;
+			$goods_type_3 = isset($_POST['goods_type_3'])?intval($_POST['goods_type_3']):0;
+			if($goods_type_3 > 0){
+				$goods_type = $goods_type_3;
+			}else{
+				if($goods_type_2 > 0){
+					$goods_type = $goods_type_2;	
+				}			
+			}
+			$data['goods_type'] = $goods_type;
+			
+            if($_FILES){
+                $images = com_save_file($_FILES,'/Upload/Ttf/goods/origin');
+                foreach($images as $key=>$val){
+                    $data['goods_logo'] = com_make_thumb($val,'/Upload/Ttf/goods/thumb');
+                    $all_data[] = $data;
+                }
+            }
+			$data['goods_desc'] = isset($_POST['goods_desc'])?trim($_POST['goods_desc']):'';
+			$data['goods_attack'] = isset($_POST['goods_attack'])?intval($_POST['goods_attack']):0;
+			$data['goods_magic'] = isset($_POST['goods_magic'])?intval($_POST['goods_magic']):0;
+			$data['goods_hp'] = isset($_POST['goods_hp'])?intval($_POST['goods_hp']):0;
+			$data['goods_mp'] = isset($_POST['goods_mp'])?intval($_POST['goods_mp']):0;
+			$data['goods_attack_defense'] = isset($_POST['goods_attack_defense'])?intval($_POST['goods_attack_defense']):0;
+			$data['goods_magic_defense'] = isset($_POST['goods_magic_defense'])?intval($_POST['goods_magic_defense']):0;
+			$data['goods_dodge'] = isset($_POST['goods_dodge'])?intval($_POST['goods_dodge']):0;
+			$data['goods_direct'] = isset($_POST['goods_direct'])?intval($_POST['goods_direct']):0;
+			$data['goods_crit'] = isset($_POST['goods_crit'])?intval($_POST['goods_crit']):0;
+			
+			$data['goods_time'] = date('Y-m-d H:i:s',time());
+			
+            if(D('goods')->goods_add($data)){
+                $this->success('添加成功!',U('currency/goods'));
             }else{
                 $this->error('添加失败');
             }
         }else{
-
-            $role = M('admin_role')->select();
-            $this->assign('role',$role);
+			$types = D('type')->type_list();
+            $this->assign('types',$types);
             $this->display();
-        }
-    }
-
-
-    //修改管理员信息
-    public function admin_edit(){
+        }			
+	}	
+	
+	public function goods_edit(){
+		
         if($_POST){
-            $admin_id = $_POST['admin_id'];
-
-            $data['admin_name'] = trim($_POST['admin_name']);
-            $data['admin_sex'] = $_POST['admin_sex'];
-            $data['admin_role_id'] = $_POST['admin_role_id'];
-            $data['admin_phone'] = $_POST['admin_phone'];
-            $data['admin_email'] = trim($_POST['admin_email']);
-            $data['admin_birthday'] = $_POST['admin_birthday'];
-            $data['admin_tag'] = trim($_POST['admin_tag']);
-
-            if(D('admin')->edit_admin($admin_id,$data)){
-                $this->success('修改成功!',U('admin/admin_list'));
-            }else{
-                $this->error('修改失败');
-            }
-        }else {
-            $admin_id = $_GET['id'] ? $_GET['id'] : '';
-            if (empty($admin_id)) {
-                $this->error('没有此管理员');
-                exit;
-            }
-
-            $admin_info = M('admin')->where('admin_id='.$admin_id)->find();
-            $roles_arr = M('admin_role')->field('role_id,role_name')->select();
-            $roles = array();
-     	
-			if($admin_info['admin_account'] == 'admin'){
-				$roles[0] = '超级管理员';
-			}else{
-				if($roles_arr){
-					foreach($roles_arr as $val){
-						$roles[$val['role_id']] = $val['role_name'];
-					}
-				}			
+            $goods_id = isset($_POST['goods_id']) ? intval($_POST['goods_id']) : 0;
+			if(empty($goods_id)) {
+				$this->error('没有此物品');
+				exit;
 			}
 
-            $sex_state = array(
-                '0' => '未知',
-                '1' => '男',
-                '2' => '女'
-            );
+			$data['goods_name'] = isset($_POST['goods_name'])?trim($_POST['goods_name']):'';
+			$goods_type = isset($_POST['goods_type'])?intval($_POST['goods_type']):0;
+			$goods_type_2 = isset($_POST['goods_type_2'])?intval($_POST['goods_type_2']):0;
+			$goods_type_3 = isset($_POST['goods_type_3'])?intval($_POST['goods_type_3']):0;
+			if($goods_type_3 > 0){
+				$goods_type = $goods_type_3;
+			}else{
+				if($goods_type_2 > 0){
+					$goods_type = $goods_type_2;	
+				}			
+			}
+			$data['goods_type'] = $goods_type;
+			
+            if($_FILES){
+                $images = com_save_file($_FILES,'/Upload/Ttf/goods/origin');
+                foreach($images as $key=>$val){
+                    $data['goods_logo'] = com_make_thumb($val,'/Upload/Ttf/goods/thumb');
+                    $all_data[] = $data;
+                }
+            }
+			$data['goods_desc'] = isset($_POST['goods_desc'])?trim($_POST['goods_desc']):'';
+			$data['goods_attack'] = isset($_POST['goods_attack'])?intval($_POST['goods_attack']):0;
+			$data['goods_magic'] = isset($_POST['goods_magic'])?intval($_POST['goods_magic']):0;
+			$data['goods_hp'] = isset($_POST['goods_hp'])?intval($_POST['goods_hp']):0;
+			$data['goods_mp'] = isset($_POST['goods_mp'])?intval($_POST['goods_mp']):0;
+			$data['goods_attack_defense'] = isset($_POST['goods_attack_defense'])?intval($_POST['goods_attack_defense']):0;
+			$data['goods_magic_defense'] = isset($_POST['goods_magic_defense'])?intval($_POST['goods_magic_defense']):0;
+			$data['goods_dodge'] = isset($_POST['goods_dodge'])?intval($_POST['goods_dodge']):0;
+			$data['goods_direct'] = isset($_POST['goods_direct'])?intval($_POST['goods_direct']):0;
+			$data['goods_crit'] = isset($_POST['goods_crit'])?intval($_POST['goods_crit']):0;
+		
+            if(D('goods')->goods_edit($data,$goods_id)){
+                $this->success('修改成功!',U('currency/goods'));
+            }else{
+                $this->error('修改失败');
+            }		
+		
+        }else {
+            $goods_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+			if(empty($goods_id)) {
+				$this->error('没有此物品');
+				exit;
+			}
+			$goods_info = M('goods')->where('goods_id='.$goods_id)->find();
+			
+			
+			// $top_pid = 0;
+			// $sub_pid = 0;
+			// if($type_info['type_pid'] > 0){
+				// $top_info = M('type')->where('type_id='.$type_info['type_pid'])->find();
+				// if($top_info['type_pid'] > 0){
+					// $top_pid = $top_info['type_pid'];
+					// $sub_pid = $top_info['type_id'];
+				// }else{
+					// $top_pid = $top_info['type_id'];
+				// }
+			// }
+			// if($sub_pid > 0){
+				// $sub_types = D('type')->type_list($top_pid);
+				// $this->assign('sub_types',$sub_types);				
+			// }
+			// $this->assign('top_pid',$top_pid);
+			// $this->assign('sub_pid',$sub_pid);
 
-            $this->assign('sex_state',$sex_state);
-            $this->assign('roles',$roles);
-            $this->assign('admin_info',$admin_info);
-            $this->display();
-        }
-    }
-
-
-    //删除管理员
-    public function admin_del(){
+			$types = D('type')->type_list();		
+			$this->assign('types',$types);
+			$this->assign('goods_info',$goods_info);
+			$this->display();
+        }			
+		
+	}	
+	
+	public function goods_del(){
         $ids = isset($_POST['ids'])?$_POST['ids']:'';
         if($ids){
-            if($ids == 1){
-                echo 2;
-                exit;
-            }
-
-            $check_ids = explode(',',$ids);
-            if(in_array(1,$check_ids)){
-                echo 2;
-                exit;
-            }
-
-            if(D('admin')->del_admin($ids)){
+            if(D('goods')->goods_del($ids)){
                 echo 1;
             }
-        }
-    }
+        }			
+	}	
+	
+	
+	/********************************************************************************/
+	//技能
+	public function skill(){
+		
+		
+	}
+	
+	public function skill_add(){
+		
+		
+	}	
+	
+	public function skill_edit(){
+		
+		
+	}	
+	
+	public function skill_del(){
+		
+		
+	}	
+		
+	
+	/********************************************************************************/
+	//怪兽
+	public function monster(){
+		
+		
+	}
+	
+	public function monster_add(){
+		
+		
+	}	
+	
+	public function monster_edit(){
+		
+		
+	}	
+	
+	public function monster_del(){
+		
+		
+	}		
+	
 
 
 
---
+++ App/Ttfadmin/Model/GoodsModel.class.php
@@ -0,0 +1,96 @@
+<?php 
+namespace Ttfadmin\Model;
+use Think\Model;
+
+class GoodsModel extends Model {
+    var $per_page = 12;
+
+    //获取limit
+    public function get_limit($page){
+        $start_num = ($page - 1) * $this->per_page;
+        return $start_num.','.$this->per_page;
+    }
+
+    //获取物品列表
+    public function goods_list($limit,$where){
+        return M('goods')->field(C('DB_PREFIX').'goods.*,'.C('DB_PREFIX').'type.type_name')->join('left join '.C('DB_PREFIX').'type ON '.C('DB_PREFIX').'goods.goods_type = '.C('DB_PREFIX').'type.type_id')->where($where)->limit($limit)->order('goods_id desc')->select();		
+		
+    }
+
+    //获得页数
+    public function goods_page($page,$where){		
+        $total_num = M('goods')->where($where)->count();
+        $total_page = ceil($total_num/$this->per_page);
+        $cur_page = $page['page'];
+        $pre_page = $cur_page - 1;
+        if($pre_page < 1) $pre_page = 1;
+        $next_page = $cur_page + 1;
+        if($next_page > $total_page) $next_page = $total_page;
+
+        $base_purl = __ROOT__.'/index.php?m=ttfadmin&c=currency&a=goods&goods_name='.$page['goods_name'].'&start_time='.$page['start_time'].'&end_time='.$page['end_time'].'&search_id='.$page['search_id'].'&search_id_2='.$page['search_id_2'].'&search_id_3='.$page['search_id_3'];
+        $purl = $base_purl.'&p=';
+
+        $page_info .= '<span class="current">共'.$total_num.'记录--'.$total_page.'页</span>';
+        $page_info .= '<a href="'.$purl.'1">首页</a>';
+        $page_info .= '<a href="'.$purl.$pre_page.'">上一页</a>';
+        $page_info .= '<span class="current">'.$cur_page.'</span>';
+        $page_info .= '<a href="'.$purl.$next_page.'">下一页</a>';
+        $page_info .= '<a href="'.$purl.$total_page.'">尾页</a>';
+
+        return $page_info;
+    }
+			
+	
+    //删除物品
+    public function goods_del($ids){
+		//要删除其对应的子类集
+		if(empty($ids)){
+			return false;
+			exit;
+		}
+		
+		if(M('goods')->where('goods_id in ('.$ids.')')->delete()){
+			D('admin_log')->admin_log('删除物品，ID为'.$ids);
+            return true;  
+        }else{
+			return false;
+		}
+    }	
+
+	//添加物品
+	public function goods_add($data){
+		$in_id = M('goods')->add($data);
+        if($in_id){
+			D('admin_log')->admin_log('增加物品 名称:'.$data['goods_name'].'，ID为:'.$in_id);
+            return true;
+        }else{
+            return false;
+        }		
+	}		
+	
+	//修改物品
+	public function goods_edit($data,$goods_id){
+		if(M('goods')->where('goods_id='.$goods_id)->save($data)){
+			D('admin_log')->admin_log('修改物品 物品名:'.$data['goods_name'].'，ID为:'.$goods_id);			
+			return true;
+		}else{
+			return false;
+		}
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+}
